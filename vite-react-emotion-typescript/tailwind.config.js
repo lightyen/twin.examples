6bbc2a23efc2d24ef69e5f520f59978874c700ca
lightyen <lightyen0123@gmail.com>
2022-07-30 07:52:44 +0800

    esbuild

--- vite-react-emotion-typescript/tailwind.config.js
+++ vite-react-emotion-typescript/tailwind.config.js
@@ -1,5 +1,13 @@
-const { register } = require("@swc-node/register/register")
-const { readDefaultTsConfig } = require("@swc-node/register/read-default-tsconfig")
-const path = require("path")
-register(readDefaultTsConfig(path.join(__dirname, "tsconfig.json")))
-module.exports = require("./tailwind.config.ts").default
+const esbuild = require("esbuild")
+const Module = require("node:module")
+
+const result = esbuild.buildSync({
+	entryPoints: ["./tailwind.config.ts"],
+	bundle: true,
+	platform: "node",
+	write: false,
+})
+
+const mod = new Module("")
+mod._compile(new TextDecoder().decode(result.outputFiles[0].contents), "")
+module.exports = mod.exports.default
